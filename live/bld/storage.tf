b0adba626583e7f0775ef17a13aa77a87fa0e27c
aj99hall <aj99hall@gmail.com>
2026-04-11 14:37:22 +0100

    Update storage.tf

--- live/bld/storage.tf
+++ live/bld/storage.tf
@@ -1,4 +1,4 @@
-resource "google_storage_bucket" "gha_test" {
+/*resource "google_storage_bucket" "gha_test" {
   name                        = "sorrow-bld-gha-test-001"
   location                    = "EU"
   uniform_bucket_level_access = true
@@ -10,4 +10,4 @@ resource "google_storage_bucket" "gha_test" {
     env        = "bld"
     purpose    = "gha-test"
   }
-}
+}*/
