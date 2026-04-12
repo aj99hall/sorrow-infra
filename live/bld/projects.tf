4108c4403f0022a32e0c85a3c4bce040a385da51
aj99hall <aj99hall@gmail.com>
2026-04-12 08:25:44 +0100

    adding build-000 project to tf

---
+++ live/bld/projects.tf
@@ -0,0 +1,7 @@
+resource "google_project" "build-000" {
+  auto_create_network = true
+  billing_account     = "014D9F-10E3E5-84036C"
+  name                = "build"
+  org_id              = "502766438313"
+  project_id          = "build-000"
+}
